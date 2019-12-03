9dc142f4a8d9b1c1e62ac7f1c3044111a4209ca9
SungJunKang <std07209@naver.com>
2019-12-04 00:20:46 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHomePage(title: 'BabyLock'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height / 1.3;
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/main/main_inactivation.jpg'),
+            fit: BoxFit.cover
+          )
+        ),
+
+        child: Container(
+          height: deviceHeight,
+          alignment: Alignment.bottomCenter,
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+          child: GridView.count(
+          primary: false,
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 0,
+          crossAxisCount: 2,
+          
+          children: <Widget>[
+            FlatButton(
+              child: Image.asset('assets/main/main_screenlock.png'),
+              onPressed: null,
+            ),
+            FlatButton(
+              child: Image.asset('assets/main/main_unlockalert.png'),
+              onPressed: null,
+            ),
+            FlatButton(
+              child: Image.asset('assets/main/main_playlist.png'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/playlist');
+              },
+            ),
+            FlatButton(
+              child: Image.asset('assets/main/main_bluelight.png'),
+              onPressed: null,
+            ),
+            FlatButton(
+              child: Image.asset('assets/main/main_tutorial.png'),
+              onPressed: null,
+            )
+          ],
+          ),
+        ),
+      ),
+    );
+  }
+}
