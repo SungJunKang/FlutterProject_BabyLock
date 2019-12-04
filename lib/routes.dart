d7cb640a07ae270387b008f35791d803ee777705
SungJunKang <std07209@naver.com>
2019-12-04 14:07:05 +0900

    Add Route

---
+++ lib/routes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:babylock/main.dart';
+import 'package:babylock/playlist.dart';
+
+final routes = {
+  '/': (BuildContext context) => Main(),
+  '/main': (BuildContext context) => Main(),
+  '/playlist': (BuildContext context) => PlayList(), 
+};
